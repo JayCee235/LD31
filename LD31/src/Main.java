3ba298850bf5c2ddb306b0aee9b2143b6f6d1edf
JayCee235 <caudejn@rose-hulman.edu>
2014-12-05 21:03:55 -0500

    Starting code. So excited to start my first LD!

---
+++ LD31/src/Main.java
@@ -0,0 +1,22 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+	public static final int HEIGHT = 600;
+	public static final int WIDTH = HEIGHT * 4 / 3;
+	
+	public static final String TITLE = "Ludum Dare 31";
+	
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame(TITLE);
+		frame.setSize(new Dimension(WIDTH, HEIGHT));
+		
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
